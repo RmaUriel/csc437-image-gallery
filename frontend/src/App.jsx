3f2f13d7090802e8e1c1a0ef46878e084e8b863e
RmaUriel <uhernandez11404@gmail.com>
2026-03-09 21:42:30 -0700

    Lab 18 complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import { AllImages } from "./images/AllImages.jsx";
 import { ImageDetails } from "./images/ImageDetails.jsx";
 import { UploadPage } from "./UploadPage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
+import { MainLayout } from "./MainLayout.jsx";
+
+import { Routes, Route } from "react-router";
 
 function App() {
-    const POSSIBLE_PAGES = [
-        <AllImages />,
-        <ImageDetails imageId={"0"} />,
-        <UploadPage />,
-        <LoginPage />
-    ];
-
-    return POSSIBLE_PAGES[0];
+    return (
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<AllImages />} />
+                <Route path="upload" element={<UploadPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="images/:imageId" element={<ImageDetails />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
